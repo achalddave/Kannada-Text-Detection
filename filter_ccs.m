89f4b7c38b5de3d80d755d88f351c5f83f5d87e5
Achal Dave <achalddave@live.com>
2014-05-09 00:19:47 -0700

    [filter_ccs]: much faster filtering by pre-filtering small components

--- filter_ccs.m
+++ filter_ccs.m
@@ -1,59 +1,59 @@
 function [filtered] = filter_ccs(ccs, stroke_widths, im_0)
-    filtered = ccs;
     VAR_THRESH = 2;
-    h = size(ccs, 1);
-    w = size(ccs, 2);
-    num_ccs = max(max(ccs));
+    GRAD_VAR_THRESH = 4;
+
+    h = size(ccs, 1)
+    w = size(ccs, 2)
+    ccs = ccs(:);
+    filtered = ccs;
+
+    sprintf('Original num components %d', size(unique(filtered), 1))
+    % Remove components with less than 5 elements
+    tabulated = tabulate(filtered);
+    indices = find(tabulated(:, 2) <= 1);
+    filtered(ismember(filtered, indices)) = -1;
+    sprintf('Num components now: %d', size(unique(filtered), 1))
+
+    unique_ccs = unique(filtered);
+    num_ccs = size(unique_ccs, 1);
 
     % --get gradient directions--------
     [~,Gdir] = imgradient(im_0);
-    % ---------------------------------
 
     % Can index into row_vals, col_vals using a raw index to get the row,
     % column value.
     row_vals = repmat([1:h]', w, 1);
     col_vals = repmat([1:w], h, 1);
 
-    for scc_idx = 1:num_ccs
+    'Filtering components: This can take a couple minutes...'
+    for i = 1:num_ccs
         % Surprisingly, doing multiple finds is faster than looping over the
         % elements once manually...
-        curr_cc_indices = find(ccs == scc_idx);
+        scc_idx = unique_ccs(i);
+        if (scc_idx == -1) ; continue ; end
+        curr_cc_indices = find(filtered == scc_idx);
 
-        rows = [];
-        cols = [];
+        curr_cc_indices(1:min(50, end), 1)';
+        rows = row_vals(curr_cc_indices);
+        cols = col_vals(curr_cc_indices);
 
         % --initialize array of scc gradients------
-        grads = [];
-        % -----------------------------------------
+        grads = Gdir(curr_cc_indices);
 
-        curr_stroke_widths = [];
-        for i = 1:size(curr_cc_indices, 1)
-            im_idx = curr_cc_indices(i);
-            r = row_vals(im_idx);
-            c = col_vals(im_idx);
-            rows = [rows r];
-            cols = [cols c];
+        curr_stroke_widths = stroke_widths(curr_cc_indices);
 
-            % --populate scc gradient array----
-            grads = [grads Gdir(r,c)];
-            % ---------------------------------
-
-            curr_stroke_widths = stroke_widths(r, c);
-        end
-
-        h = max(rows) - min(rows);
-        w = max(cols) - min(cols);
+        curr_h = max(rows) - min(rows);
+        curr_w = max(cols) - min(cols);
 
         % --Create histogram and get bincount----
         [nelements,~] = hist(grads);
-        GRAD_VAR_THRESH = 4;
-        % ---------------------------------------
 
-        if (h < 1 || h > 300) || ...
-            ((h / w) < 0.1 || (h / w) > 10) || ...
-            (var(curr_stroke_widths) > VAR_THRESH || ...
-            (var(nelements) > GRAD_VAR_THRESH))
-            filtered(curr_cc_indices) = 0;
+        if (curr_h < 10 || curr_h > 300) || ...
+            ((curr_h / curr_w) < 0.1 || (curr_h / curr_w) > 10) || ...
+            (var(curr_stroke_widths) > VAR_THRESH) || ...
+            (var(nelements) > GRAD_VAR_THRESH)
+            filtered(curr_cc_indices) = -1;
         end
     end
+    filtered = reshape(filtered, h, w);
 end
