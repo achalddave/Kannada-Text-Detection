8798878057b2b8f77d14f59bf65999fa1b062b5a
Achal Dave <achalddave@live.com>
2014-05-10 23:48:58 -0700

    [sign_read]: update to comp_vote args

--- sign_read.m
+++ sign_read.m
@@ -2,4 +2,7 @@ IM = im2double(rgb2gray(imread('sign3.jpg')));
 [swt_im, ccomps] = swt(IM,1);
 [coarse_filt, gnt_filtered, gt, mt, g_comp_idxs, m_comp_idxs] = ...
     filter_ccs(ccomps, swt_im, IM, 1);
-PROB = comp_vote(gnt_filtered, g_comp_idxs, m_comp_idxs);
+[PROB, added, added_indices] = ...
+    comp_vote(gnt_filtered, gt, g_comp_idxs, m_comp_idxs);
+gt_final = gt;
+gt_final(added > 0) = 1;
