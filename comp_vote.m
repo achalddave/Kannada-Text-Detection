b2eda38c6dba3489fb718aecbcbfe1963d6293ee
Achal Dave <achalddave@live.com>
2014-05-10 21:29:03 -0700

    [comp_vote]: minor cleanup

--- comp_vote.m
+++ comp_vote.m
@@ -13,13 +13,14 @@ col_vals = repmat([1:w], h, 1);
 for i = 1: length(g_comp_idxs)
     scc_idx = g_comp_idxs(i);
     curr_cc_indices = find(gnt_filtered == scc_idx);
-    
+
     rows = row_vals(curr_cc_indices);
     cols = col_vals(curr_cc_indices);
-    
+
     center_h = round((max(rows) + min(rows))/2);
     center_w = round((max(cols) + min(cols))/2);
-    
+
     PROB = PROB + gauss2d(h, w, center_w, center_h, SIGMA);
 end
-    
+
+end
